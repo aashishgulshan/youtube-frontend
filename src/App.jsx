481811b4b354a7319a05f37ef3ece1adedbfbcb7
Aashish Gulshan <aashish.gulshan@zehntech.com>
2024-01-12 19:24:05 +0530

    Created layout of the application

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import BottomNavigation from "./components/BottomNavigation";
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <BottomNavigation></BottomNavigation>
+    </>
+  );
+}
+
+export default App;
